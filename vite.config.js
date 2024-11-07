75ce74c404eadaf135c4bdda703f53cd88d36232
CodeskStudio <code@codesk.studio>
2024-11-08 00:50:00 +0100

    added build option and comment

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  base: '/'  // Replace '/' with your subpath or leave as '/' if in the root
 })
